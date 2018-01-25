37283a1144b2c85618e74f01123f07de3d7ab609
mohammad-abbasi-mab <mab.abbasi1374@gmail.com>
2018-01-25 20:16:41 +0330

    almost there

--- CA2_Q1.py
+++ CA2_Q1.py
@@ -1,3 +1,71 @@
 a = input()
-for i in a:
-    print i
+operator  = ["+" , "-" , "*" , "/" , "^","(",")"]
+# oprnd = []
+# oprtr = []
+
+def pstfx(x):
+    oprnd = []
+# oprtr = []
+    for i in x:
+        if i not in operator:
+            oprnd.append(i)
+        else:
+            f = float(oprnd.pop())
+            s = float(oprnd.pop())
+            if i == "+":
+                r = f+s
+
+            if i == "*":
+                r = f*s
+
+            if i == "/":
+                r = f/s
+
+            if i == "-":
+                r = f-s
+
+            if i == "^":
+                r = s**f
+            oprnd.append(r)
+    return oprnd.pop()
+
+
+def infx(x):
+    oprnd = []
+    oprtr = []
+    prec = {}
+    prec["*"] = 3
+    prec["/"] = 3
+    prec["+"] = 2
+    prec["-"] = 2
+    prec["("] = 1
+    for i in x:
+        if i not in operator:
+            oprnd.append(i)
+        elif i == "(":
+            oprtr.append(i)
+        elif i == ")":
+            topToken = oprtr.pop()
+            while topToken != '(':
+                oprnd.append(topToken)
+                topToken = oprtr.pop()
+        else:
+            while (oprtr != []) and \
+               (prec[oprtr[-1]] >= prec[i]):
+                  oprnd.append(oprtr.pop())
+            oprtr.append(i)
+
+
+
+    while oprtr != []:
+        oprnd.append(oprtr.pop())
+    return " ".join(oprnd)
+
+
+
+
+
+if a[-1] in operator and a[-1] != ")":
+    print (pstfx(a))
+else:
+    print (infx(a))
